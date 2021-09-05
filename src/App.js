dec5951eb084401c3d6f0ba1e00ece91ccbd999f
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-09-06 01:08:21 +0530

    beta

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import "./App.css";
+import ReactMarkdown from "react-markdown";
 
 function App() {
+  const [markdown, setMarkdown] = useState("# heading one");
   return (
     <main>
       <div className="container">
         <section className="markdown">
-          <h2>Markdown :</h2>
-          <textarea className="textarea"></textarea>
+          <h2 className="heading">Markdown :</h2>
+          <textarea
+            className="textarea"
+            onChange={(e) => setMarkdown(e.target.value)}
+            value={markdown}
+          ></textarea>
         </section>
         <section className="results">
-          <h2>Results :</h2>
+          <h2 className="heading">Results :</h2>
+          <div className="results-box">
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </div>
         </section>
       </div>
     </main>
