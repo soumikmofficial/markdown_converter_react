8e086c212b1b70862db35c44a690d6188ec4d7f7
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-09-05 20:36:34 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        <section className="markdown">
+          <h2>Markdown :</h2>
+          <textarea className="textarea"></textarea>
+        </section>
+        <section className="results">
+          <h2>Results :</h2>
+        </section>
+      </div>
+    </main>
   );
 }
 
